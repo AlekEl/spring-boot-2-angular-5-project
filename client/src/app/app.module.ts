4833e77c4b8d10a39f20acd1931d81f13c26b749
AlekEl <leszczynski.al@gmail.com>
2019-03-07 14:30:20 +0100

    Add car-list to angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarService} from "./shared/car/car.service";
+import { CarListComponent } from './car-list/car-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent
   ],
   imports: [
     BrowserModule,
