350d49b5086804ecbd4222dc6f69c8b7f028fd58
AlekEl <leszczynski.al@gmail.com>
2019-03-07 15:01:50 +0100

    Add spring to angular communication

--- src/main/java/com/codecool/demo/Controller/CoolCarController.java
+++ src/main/java/com/codecool/demo/Controller/CoolCarController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 class CoolCarController {
@@ -16,6 +17,7 @@ class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
