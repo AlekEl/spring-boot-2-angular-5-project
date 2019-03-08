f1d548a72307f99722b9dc900f69eb048e6f4e2f
AlekEl <leszczynski.al@gmail.com>
2019-03-08 10:39:32 +0100

    Updated car-service

--- client/src/app/shared/car/car.service.ts
+++ client/src/app/shared/car/car.service.ts
@@ -6,8 +6,14 @@ import { Observable } from 'rxjs/Observable';
   providedIn: 'root'
 })
 export class CarService {
+  public API = '//localhost:8080';
+  public CAR_API = this.API + '/cars';
+
   getAll(): Observable<any> {
-    return this.http.get('//localhost:8080/cool-cars');
+    return this.http.get(this.API + '/cool-cars');
+  }
+  get(id: string) {
+    return this.http.get(this.CAR_API + '/' + id)
   }
 
   constructor(private http: HttpClient) { }
